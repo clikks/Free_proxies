904d21c7cbbc5c6c37f36c57685a8d882494701b
clikks <anycosmo@live.com>
2017-09-21 00:15:21 +0800

    success get all proxies ip

--- proxie_list.py
+++ proxie_list.py
@@ -3,64 +3,88 @@
 __author__ = 'anyco'
 
 import requests
-import time
-import os
+import threading
 from lxml import etree
 from proxies_init import Proxiesinit
+from time import sleep
 
 
 class Proxiesdata:
     def __init__(self):
-        self.Instance = Proxiesinit()       # 实例化Proxiesinit类
+        # self.lock = threading.Lock()
+        self.Instance = Proxiesinit(r'D:\webdrive\phantomjs\bin\phantomjs.exe')       # 实例化Proxiesinit类
         self.header = self.Instance.get_header()    # 将Proxiesinit()的get_header方法赋值给header
         self.page_num, self.index = self.Instance.page_count()   # 将page_count方法的值赋值给page_num和index
         self.proxy_page = 'http://www.kuaidaili.com/free/inha/%s/'  # 定义proxy_page的网页url
+        # self.req = requests.session()
+        self.dbase = list()
+        self.failed = list()
 
     def proxy_url(self):
-        self.pagenum = [i for i in range(1,self.page_num+1)]
-        s = int(self.page_num/100) + 1
-        self.allurl = list()
-        for i in range(s):
-            self.allurl.append(self.pagenum[0:100])
-            del self.pagenum[0:100]
-        print(self.allurl)
+        self.pagenum = [i for i in range(1, self.page_num+1)]
+        self.allurl = [self.pagenum[i:i+100] for i in range(0, self.page_num+1, 100)]
+        # print(self.allurl)
+        return self.allurl
 
-    def analyze_page(self):     # 代理页面解析方法
-        os.system('cls')        # 清屏
-        print("Analyze webpage's data,please wait...")
-        self.dbase = list()
-        req = requests.session()
-        for num in range(1, self.page_num):
-            print(num)
-            ctl = True
-            xpath_rule = '//*[@id="list"]/table/tbody/tr/td[1]/text() | //*[@id="list"]/table/tbody/tr/td[2]/text()'
+    def get_ip(self, pagelist):
+        self.pagelist = pagelist
+        self.req = requests.session()
+        xpath_rule = '//*[@id="list"]/table/tbody/tr/td[1]/text() | //*[@id="list"]/table/tbody/tr/td[2]/text()'
+        # count = 0
+        for num in self.pagelist:
+            print('Start analyze page %d ...' %num)
             if num == 1:
-                print(self.index.text)
-                page_data = etree.HTML(self.index.text)
+                pagedate = etree.HTML(self.index.text)
             else:
                 url = self.proxy_page %num
-                page = req.get(url, headers=self.header, timeout=5)
-                page_data = etree.HTML(page.text)
-                count = 1
-                while page.status_code != 200:
-                    print('Request Failed! RESTART...')
-                    self.header = self.Instance.get_header()
-                    page_data = etree.HTML(req.get(url, headers=self.header, timeout=5).text)
-                    count += 1
+                try:
+                    page = self.req.get(url, headers=self.header, timeout=10)
+                    count = 1
+                    while page.status_code != 200:
+                        print('Page %d %d times request Failed! Re request!' %(num, count))
+                        self.header = self.Instance.get_header()
+                        page = self.req.get(url, headers=self.header, timeout=30)
+                        count += 1
+                        if count > 10:
+                            self.failed.append(num)
+                            break
                     if count > 10:
-                        print('Content page number is %d,can not request correct page source!')
-                        ctl = False
-                        break
-            if ctl is False:
-                break
-            result = page_data.xpath(xpath_rule)
-            for ip in range(0, len(result), 2):
-                self.dbase.append(result[ip] + ':' + result[ip + 1])
-            time.sleep(3)
+                        print('Page %d can not access!Continue request next page!' %num)
+                        continue
+                    else:
+                        pagedate = etree.HTML(page.text)
+                    result = pagedate.xpath(xpath_rule)
+                    for ip in range(0, len(result), 2):
+                        self.dbase.append(result[ip]+':'+result[ip+1])
+                # sleep(3)
+                except:
+                    print('Network Error!Please check it!')
+
+    def mutil_thread(self):
+        urladdress = self.proxy_url()
+        thread_num = []
+        tnum = 1
+        for i in urladdress:
+            try:
+                print('Thread %d set up!' %tnum)
+                t = threading.Thread(target=self.get_ip, args=(i,))
+                thread_num.append(t)
+                t.start()
+                tnum += 1
+            except:
+                print('error')
+        for thread in thread_num:
+            thread.join()
+            print('Thread finished!')
+
+        print('All Thread finished!')
         return self.dbase
 
 if __name__ == '__main__':
     ClassInstance = Proxiesdata()
     print(ClassInstance.page_num)
     # print(ClassInstance.analyze_page())
-    ClassInstance.proxy_url()
+    # ClassInstance.proxy_url()
+    ClassInstance.mutil_thread()
+    print(ClassInstance.failed)
+    print(ClassInstance.dbase)
--- proxies_init.py
+++ proxies_init.py
@@ -3,98 +3,84 @@
 __author__ = 'anyco'
 
 import requests
-import time
-import os
 import sys
-import platform
+import threading
 from lxml import etree
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 class Proxiesinit:
-    def __init__(self, phantomjsPath=None):
+    def __init__(self, phantomjsPath):
         self.phantomjsPath = phantomjsPath
-        print('{}Inital program ready!{}'.format('='*10,  '='*10))
-        # self.pagination = pagination
-        self.index = 'http://www.kuaidaili.com/free/'    # index代理主页地址
-        # self.proxy_page = 'http://www.kuaidaili.com/free/inha/%s/' %(self.pagination)
-        # proxy_page其余页面地址,pagination为页数
+        self.count = 1
+        print('{}Inital program ready!{}'.format('=' * 10, '=' * 10))
+        self.index = 'http://www.kuaidaili.com/free/'  # index代理主页地址
         self.header = {
-                        'Accept': '*/*',
-                        'Accept-Encoding': 'gzip, deflate, sdch',
-                        'Accept-Language': 'zh-CN,zh;q=0.8',
-                        'Connection': 'keep-alive',
-                        'Cookie': '',
-                        'Host': 'www.kuaidaili.com',
-                        'Referer': 'http://www.kuaidaili.com/free',
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, sdch',
+            'Accept-Language': 'zh-CN,zh;q=0.8',
+            'Connection': 'keep-alive',
+            'Cookie': '',
+            'Host': 'www.kuaidaili.com',
+            'Referer': 'http://www.kuaidaili.com/free',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
                         (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
-                        'X-Requested-With': 'XMLHttpRequest',
-                        }
+            'X-Requested-With': 'XMLHttpRequest',
+        }
+        self.params = dict(DesiredCapabilities.PHANTOMJS)  # PhantomJS类属性，为字典类型
+        self.params["phantomjs.page.settings.userAgent"] = (self.header['User-Agent'])
+        self.params["phantomjs.page.settings.loadImages"] = False  # 定义PhantomJS的UserAgent及关闭图片加载
 
     def get_header(self):
-        params = dict(DesiredCapabilities.PHANTOMJS)
-        # PhantomJS类属性，为字典类型
-        params["phantomjs.page.settings.userAgent"] = (self.header['User-Agent'])
-        # 定义PhantomJS的UserAgent
-        print('Confirming the system platform type...')
-        sys = platform.system()                         # 获取操作系统类型
-        # print("Calling {}'s PhantomJS path!".format(sys))
-        if sys == 'Windows':
-            self.phantomjsPath = r'D:\webdrive\phantomjs\bin\phantomjs.exe'
-            # self.phantomjsPath = r'd:\phan.exe'
-        elif sys == 'Linux':
-            self.phantomjsPath = r'/usr/local/bin/phantomjs'
-        # else:
-        #     self.phantomjsPath = phantomjsPath
-        driver = webdriver.PhantomJS(executable_path=self.phantomjsPath, desired_capabilities=params)
+        lock = threading.Lock()
+        lock.acquire()
+        driver = webdriver.PhantomJS(executable_path=self.phantomjsPath, desired_capabilities=self.params)
         # seltnium的webdriver，提供phantomJS的路径和UserAgent.
-        driver.get(self.index)                          # 网页请求
-        time.sleep(3)                                   # 等待3秒
-        os.system('cls')
-        print("Calling {}'s PhantomJS to analyze webpage...".format(sys))
-        driver.refresh()                                # 刷新页面
-        cookies = driver.get_cookies()                  # 获得cookie列表
-        # print(cookies)
-        items = []
-        for value in cookies:
-            # print(value)
-            content = [value['name'], value['value']]    # 重组获得的cookie
-            items.append('='.join(content))             # 格式化cookie内容
-            # print(items)
-        self.header['Cookie'] = '; '.join(items)        # 将格式化的cookie赋值给header内
+        driver.get(self.index)  # 网页请求
+        sleep(3)
+        if self.count == 1:
+            print("Calling PhantomJS to analyze webpage...".format(sys))
+        else:
+            print('Get new header!')
+        cookies = driver.get_cookies()  # 获得cookie列表
+        items = [value['name'] + '=' + value['value'] for value in cookies]  # 格式化cookie内容
+        # print(items)
+        self.header['Cookie'] = '; '.join(items)  # 将格式化的cookie赋值给header内
         # print(self.header['Cookie'])
-        driver.close()
+        driver.quit()
+        self.count += 1
+        lock.release()
         return self.header
 
     def page_count(self):
-        req = requests.session()
-        while True:
-            try:
-                html_page = req.get(self.index, headers=self.header, timeout=5) # 发起网页请求
-                # print(self.header)
-                # print(html_page.status_code)
-                if html_page.status_code != 200:    # 如果请求失败，重新获得header并发起请求
-                    print('Request status code is %d' % html_page.status_code)
-                    time.sleep(60)
-                    self.get_cookie()
-                    continue
-                else:
-                    print('Analyze index complete!')
-                    result = etree.HTML(html_page.text)     # 解析请求到的页面
-                    pagecount = result.xpath('/html/body/div[@class="body"]/div[@id="content"]/div[@class="con-body"]\
-                    /div/div[@id="list"]/div[@id="listnav"]/ul/li[last()-1]/a/text()')
-                    # print(int(pagecount[0]))
-                    return int(pagecount[0]),html_page
-                    # break
+        self.required = requests.session()
+        try:
+            html_page = self.required.get(self.index, headers=self.header, timeout=10)  # 发起网页请求
+            # print(self.header)
+            # print(html_page.status_code)
+            status = html_page.status_code
+            while status != 200:
+                print('Status code is %d.' % status)
+                sleep(15)
+                self.get_header()
+                html_page = self.required.get(self.index, headers=self.header, timeout=30)
+                status = html_page.status_code
+            else:
+                print('Analyze index page complete!')
+                result = etree.HTML(html_page.text)
+                pagecount = result.xpath('/html/body/div[@class="body"]/div[@id="content"]/div[@class="con-body"]\
+                                    /div/div[@id="list"]/div[@id="listnav"]/ul/li[last()-1]/a/text()')
+                return int(pagecount[0]), html_page
 
-            except:
-                input('Error：please check network!Press any key to exit!')
-                sys.exit()
+        except:
+            input('Error：please check network!Press any key to exit!')
+            sys.exit(socket.timeout,)
+
 
 if __name__ == '__main__':
-    c1 = Proxiesinit()
-    c1.get_header()
+    c1 = Proxiesinit(r'D:\webdrive\phantomjs\bin\phantomjs.exe')
+    print(c1.get_header())
     X = c1.page_count()
     print(X[0])
